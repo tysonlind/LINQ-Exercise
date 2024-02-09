9d4ffca94ef500ab0ee9c482010c5005d8179746
tyson lind <tysonfloydlind@gmail.com>
2024-02-08 22:23:51 -0700

    corrected

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,8 +2,10 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<string> strings = new List<string>() {"Final Fantasy I", "Doom", "Monster Rancher", "Galactica", "Faster Than Light", "GTA 5" };
+            IOrderedEnumerable<string> orderedStrings = strings.OrderByDescending(x => x.Length);
             Console.WriteLine("Hello, World!");
         }
     }
